8f3c5f754a40b478d430e1d263dc3da0f54cc71d
Dattu Gollapinni <101050977+dattu20038@users.noreply.github.com>
2025-02-15 09:26:44 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -119,7 +119,7 @@ def main():
             """
             
             HUGGINGFACE_REPO_ID = "deepseek-ai/deepseek-7b-chat"
-            HF_TOKEN = st.secrets["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"]
+            HF_TOKEN = "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"
             
             try:
                 vectorstore = get_vectorstore()
